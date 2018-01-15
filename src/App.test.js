462f4e200b1d8b828599b323beeaaa8c733d4aaa
Edmund Mai <edmundmai@gmail.com>
2018-01-15 10:54:03 -0500

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+describe("App works", () => {
+  it("returns true", () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
